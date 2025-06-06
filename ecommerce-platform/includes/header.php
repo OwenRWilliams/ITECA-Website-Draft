c88db09f9759c89f1ceab5eb900914690a018ae7
Owen Williams <orwilliams2004@icloud.com>
2025-06-06 18:11:10 +0200

    Add files via upload

--- ecommerce-platform/includes/header.php
+++ ecommerce-platform/includes/header.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['CREATED'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Informal Market</title>
+    <title>Nozama</title>
     <!-- Font and Icon Links -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
